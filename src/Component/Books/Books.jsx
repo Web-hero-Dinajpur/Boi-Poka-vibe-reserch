e95468357f69cb74e5c50289195b6cf1397136c4
Sreedhar-roy <sreedharroy0@gmail.com>
2025-05-02 09:46:21 +0600

    books section data setup

--- src/Component/Books/Books.jsx
+++ src/Component/Books/Books.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import { useState, useEffect } from "react";
+
 
 const Books = () => {
+
+    const [Books, setBooks] = useState([]);
+
+    useEffect(() => {
+        fetch('./booksData.json')
+            .then(res => res.json())
+            .then(data => setBooks(data))
+    }, [])
+
     return (
         <div className='my-9'>
             <h2 className="text-4xl font-bold text-center">Books</h2>
+            <h2>{Books.length}</h2>
+
         </div>
     );
 };
