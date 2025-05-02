7b3145720631036afd3db7eb552a1a2a3365b0d7
Sreedhar-roy <sreedharroy0@gmail.com>
2025-05-02 09:06:07 +0600

    boi poka book resourch first commit

---
+++ src/Component/ErrorPages/ErrorPages.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ErrorPages = () => {
+    return (
+        <div>
+            <p className="font-5">Pages Is Not Found</p>
+        </div>
+    );
+};
+
+export default ErrorPages;
---
+++ src/Component/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h2 className="font-bold">this is home pages</h2>
+        </div>
+    );
+};
+
+export default Home;
