0c93fd09431d1765047db6a11dbf2974b6c842a6
Sreedhar-roy <sreedharroy0@gmail.com>
2025-05-04 11:59:48 +0600

    card section book commit done

--- src/Component/Books/Books.jsx
+++ src/Component/Books/Books.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import Book from "../Book/Book";
 
 
 const Books = () => {
@@ -14,7 +15,11 @@ const Books = () => {
     return (
         <div className='my-9'>
             <h2 className="text-4xl font-bold text-center">Books</h2>
-            <h2>{Books.length}</h2>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 ">
+                {
+                    Books.map(book => <Book book={book} key={book.bookId}></Book>)
+                }
+            </div>
 
         </div>
     );
